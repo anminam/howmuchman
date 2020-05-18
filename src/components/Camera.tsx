a15983c582cba0c98ea4e533b3ee8aae6d265926
Anminam <minamhunmin@gmail.com>
2020-05-18 16:09:27 +0900

    update camera component

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -3,33 +3,94 @@ import React, { useRef, useEffect, useState } from 'react';
 const Camera = () => {
 
   const videoEl = useRef<HTMLVideoElement>(null);
+  const [cameraStream, setCameraStream] = useState<MediaStream>();
   const [errMessage, setErrorMessage] = useState<String>('');
+  const [deviceList, setDeviceList] = useState<MediaDeviceInfo[]>([]);
+
+  // 비디오 설정
+  const initMediaConfig = {
+    video: {
+      facingMode: {
+        exact: "user"
+        // exact: "environment"
+      }
+    }
+  }
+
+  const [mediaConfig, setMediaConfig] = useState(initMediaConfig);
 
   /**
-   * 이미지 받아오기
+   * 권한 받기 성공
    * @param stream 
    */
-  const handleSuccess = (stream) => {
+  const handleSuccess = (stream: MediaStream) => {
     if (null !== videoEl.current) {
+      setCameraStream(stream);
       videoEl.current.srcObject = stream;
+    }
+
+    navigator.mediaDevices.enumerateDevices().then(getDivices);
+  }
+
+  /**
+   * 
+   * @param mediaDevices 
+   */
+  const getDivices = (mediaDevices: MediaDeviceInfo[]) => {
+    const addDevices: MediaDeviceInfo[] = [];
+    mediaDevices.forEach((device: MediaDeviceInfo, i) => {
+      if (device.kind === 'videoinput') {
+        addDevices.push(device);
+      }
+    });
+    setDeviceList([...addDevices])
+  }
+
+  const onClickCameraKind = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    stopCamera();
+
+    const config = 'environment';
+    setMediaConfig({
+      ...mediaConfig,
+      video: {
+        facingMode: {
+          exact: config
+        }
+      }
+    })
+  }
+
+  const stopCamera = () => {
+    if (undefined !== cameraStream) {
+      cameraStream.getTracks().forEach(i => {
+        i.stop();
+      });
     }
   }
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true })
+    // 권한체크
+    navigator.mediaDevices.getUserMedia(mediaConfig)
       .then(handleSuccess)
       .catch((err) => {
-        if (err.name === 'NotFoundError') {
-          setErrorMessage('카메라를 찾을 수 없습니다.');
+        switch (err.name) {
+          case "NotFoundError":
+            setErrorMessage('카메라를 찾을 수 없습니다.');
+            break;
+          default:
+            setErrorMessage('알 수 없는 에러');
         }
       });
-  }, []);
+  }, [mediaConfig]);
 
-
+  console.log('이게', deviceList);
   return (
     <div>
       <video id="player" ref={videoEl} controls autoPlay />
       <p>{errMessage}</p>
+      <div>
+        <button onClick={onClickCameraKind}>꾸앙</button>
+      </div>
     </div>
   )
 }
