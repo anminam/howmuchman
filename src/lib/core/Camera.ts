c64d10b74e22ef7bb1d0e9cc7cd246c20c07fb40
Anminam <minamhunmin@gmail.com>
2020-05-21 17:24:56 +0900

    camera lib

--- src/lib/core/Camera.ts
+++ src/lib/core/Camera.ts
@@ -32,57 +32,58 @@ type IVideoType = HTMLVideoElement | React.RefObject<HTMLVideoElement>;
 
 export interface ICamera {
     videoElement: IVideoType
+    captureElement?: React.RefObject<HTMLButtonElement>
+    changeCameraElement?: React.RefObject<HTMLButtonElement>
+    direction?: IDirection
+    size?: {
+        width: number
+        height: number
+    }
 }
 
 class Camera {
-
-    // 비디오 설정
-    initMediaConfig:IMediaConfig = {
-        video: {
-        width: { ideal: 1280 },
-        height: { ideal: 1024 },
-        facingMode: {
-            exact: ICameraDirectionConfig.ENVIRONMENT
-        },
-        // mandatory: {
-        //   minWidth: 1280,
-        //   minHeight: 720
-        // }
-        }
-    }
 
     videoElement:IVideoType;
 
     stream?:MediaStream;
     devices?:MediaDeviceInfo[];
 
+    captureElement?: React.RefObject<HTMLButtonElement>
+    changeCameraElement?: React.RefObject<HTMLButtonElement>
+    direction: IDirection;
+
     constructor(props:ICamera) {
-        this.videoElement = props.videoElement;
+        const {videoElement, captureElement, changeCameraElement, direction } = props;
+        this.videoElement = videoElement;
+        this.captureElement = captureElement;
+        this.changeCameraElement = changeCameraElement;
+        this.direction = direction || IDirection.BACK
+    }
+
+    _getVideoConfig = ():IMediaConfig => {
+        return {
+            video: {
+            width: { ideal: 1280 },
+            height: { ideal: 1024 },
+            facingMode: {
+                exact: this._getDirection(this.direction)
+            },
+            // mandatory: {
+            //   minWidth: 1280,
+            //   minHeight: 720
+            // }
+            }
+        }
     }
 
     /**
-   * videoDevice 가져오기
-   * @param mediaDevices 
-   */
-    _getDivices = (mediaDevices: MediaDeviceInfo[]) => {
-        const addDevices: MediaDeviceInfo[] = [];
-        mediaDevices.forEach((device: MediaDeviceInfo, i) => {
-        if (device.kind === 'videoinput') {
-            addDevices.push(device);
-        }
-        });
-        setDeviceList([...addDevices]);
-        if (addDevices.length > 1) {
-        setIsMulticamera(true);
-        }
-
-    }
-
+     * 퍼미션 체크
+     */
     _checkMediaPermission = async () => {
         let stream: MediaStream | null = null;
 
         try {
-            stream = await navigator.mediaDevices.getUserMedia(this.initMediaConfig);
+            stream = await navigator.mediaDevices.getUserMedia(this._getVideoConfig());
         } catch(error) {
             switch (error.name) {
                 case "NotFoundError":
@@ -96,6 +97,9 @@ class Camera {
         return stream;
     }
 
+    /**
+     * 비디오 스트림 연결
+     */
     _connectStream = () => {
         if(!this.stream) {
             return;
@@ -106,7 +110,10 @@ class Camera {
         }
     }
 
-    _getDevice = async () => {
+    /**
+     * 디바이스 가져오기
+     */
+    _getDivices = async () => {
         const addDevices: MediaDeviceInfo[] = [];
         try {
             const devices = await navigator.mediaDevices.enumerateDevices()
@@ -123,42 +130,77 @@ class Camera {
     }
 
     /**
-     * 
+     * props에서 얻은값 -> ICameraDirectionConfig
+     * @param configDirection 
+     */
+    _getDirection = (configDirection:IDirection):ICameraDirectionConfig => {
+        let direction: ICameraDirectionConfig = ICameraDirectionConfig.ENVIRONMENT
+        switch(configDirection) {
+        case IDirection.FRONT: 
+            direction = ICameraDirectionConfig.USER;
+            break;
+        case IDirection.BACK: 
+            direction = ICameraDirectionConfig.ENVIRONMENT;
+            break;
+        default:
+            break;
+        }
+    
+        return direction;
+    }
+  
+
+    /**
+     * 카메라 시작
      */
     startCamera = async () => {
         try {
-            await this.stopCamera();
+
+            this.stopCamera();
 
             this.stream = await this._checkMediaPermission();
 
             this._connectStream();
 
-            this.devices = await this._getDevice();
-
-
-
-
-
-
+            this.devices = await this._getDivices();
 
         } catch (error) {
             console.log(error.toString());
         }
     }
+
     /**
-     * 
+     * 카메라 중단
      */
-    stopCamera = async () => {
+    stopCamera = () => {
         try {
-
+            this.stream?.getTracks().forEach(i => {
+                i.stop();
+            });
         } catch (error) {
-
+            console.log(error.toString());   
         }
-
     }
 
-    
+    /**
+     * 카메라 바꾸기
+     */
+    changeCamera = () => {
 
+        this.stopCamera();
+
+        const configList:Array<ICameraDirectionConfig> = [ICameraDirectionConfig.ENVIRONMENT, ICameraDirectionConfig.USER];
+        const deviceLength = configList.length;
+        let nextIndex = this.direction + 1;
+        
+        if (deviceLength === nextIndex) {
+            nextIndex = 0;
+        }
+
+        this.direction = nextIndex;
+
+        this.startCamera();
+    }
 }
 
 export default Camera;
